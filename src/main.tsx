0140d6047dfce566d89ce3d024e98aa871e016f1
Skyler Riley <skylerriley1@gmail.com>
2026-06-19 11:57:16 -0700

    analytics tracking

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 
@@ -21,5 +22,6 @@ window.addEventListener('resize', applySafeAreaInsets);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
